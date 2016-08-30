3c187caee339138dd25bc4f06d2ae5c7571f2a8e
Jens Gabrikowski <jens@gabrikowksi.de>
2016-08-30 19:14:06 +0200

    Multiple Components - https://angular.io/docs/ts/latest/tutorial/toh-pt3.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
     selector: 'my-app',
     template:`
       <h1>{{title}}</h1>
-      <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-        </div>
-      </div>
 
       <h2>My Heroes</h2>
       <ul class="heroes">
@@ -21,6 +14,8 @@ import { Component } from '@angular/core';
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
+
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
     styles: [`
       .selected {
@@ -81,11 +76,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
